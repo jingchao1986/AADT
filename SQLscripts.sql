f6893637139e8cbbb39acb8a30869ba9ee226a19
Jingchao Zhou <jingchao1986@gmail.com>
2020-10-26 10:26:26 -0700

    change file name to aadt

--- SQLscripts.sql
+++ SQLscripts.sql
@@ -1,25 +1,14 @@
-CREATE TABLE Nodes
-(
-    id serial NOT NULL PRIMARY KEY,
-    Name VARCHAR(10)
-);
+CREATE TABLE Nodes (id serial NOT NULL PRIMARY KEY,
+                                               Name VARCHAR(10));
 
 
-CREATE TABLE EdgeClass
-(
-    id serial NOT NULL PRIMARY KEY,
-    class VARCHAR(100)
-);
+CREATE TABLE EdgeClass (id serial NOT NULL PRIMARY KEY,
+                                                   class VARCHAR(100));
 
 
-CREATE TABLE Edge
-(
-    id serial NOT NULL PRIMARY KEY,
-    class int,
-    startPoint int,
-    endPoint int,
-    foreign key (class) references edgeClass(id),
-    foreign key (startPoint) references Nodes(id),
-    foreign key (endPoint) references Nodes(id)
-);
+CREATE TABLE Edge (id serial NOT NULL PRIMARY KEY,
+                                              class int, startPoint int, endPoint int, length int,
+                   foreign key (class) references edgeClass(id),
+                   foreign key (startPoint) references Nodes(id),
+                   foreign key (endPoint) references Nodes(id));
 
