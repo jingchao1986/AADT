b034a63f15818987c4c31bf9ae2cfad661f6dfb0
Jingchao Zhou <jingchao1986@gmail.com>
2020-10-26 09:45:26 -0700

    initilized addt

---
+++ SQLscripts.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Nodes
+(
+    id serial NOT NULL PRIMARY KEY,
+    Name VARCHAR(10)
+);
+
+
+CREATE TABLE EdgeClass
+(
+    id serial NOT NULL PRIMARY KEY,
+    class VARCHAR(100)
+);
+
+
+CREATE TABLE Edge
+(
+    id serial NOT NULL PRIMARY KEY,
+    class int,
+    startPoint int,
+    endPoint int,
+    foreign key (class) references edgeClass(id),
+    foreign key (startPoint) references Nodes(id),
+    foreign key (endPoint) references Nodes(id)
+);
+
